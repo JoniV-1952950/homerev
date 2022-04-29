04eea4bd53b5152a4f79d6964870a4d35152629e
jonivanherck <joni.vanherck@student.uhasselt.be>
2022-04-29 10:05:02 +0200

    js immediately invoked function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,72 +20,78 @@ import { resolvers } from './resolvers/resolvers';
 import { authDirective } from './schema/auth'; 
 import { Variables } from "./utils/variables";
 
-const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective();
+// javascript immediately invoked function to not have the need for top level await
+(async function() {
 
-// create dataSources object
-const dataSources = () => ({
-    usersAPI: new UsersAPI(),
-    medAPI: new MedAPI()
-});
-// get the project types
-const projectTypesArr = ["bimanueel", "VR"]; //await dataSources().medAPI.getProjectTypes(); // does not work: await at top level => ES2022 compile option gives other problems
-let projectTypes = `enum ProjectType {`;
-for(const projectType of projectTypesArr) {
-  projectTypes += projectType + `,`;
-}
-projectTypes += `}`; 
+  // get the authdirective to merge it with the schema soon
+  const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective();
 
-// create schema, using type definitions in schema.ts, auth directive declaration from auth.ts, projectTypes enum and the resolvers
-let schema = makeExecutableSchema({
-  typeDefs: [
-    authDirectiveTypeDefs,
-    queries,
-    mutations,
-    projectTypes
-  ],
-  resolvers,
-});
-// transform the schema so that the auth directive is implemented
-schema = authDirectiveTransformer(schema);
+  // create dataSources object
+  const dataSources = () => ({
+      usersAPI: new UsersAPI(),
+      medAPI: new MedAPI()
+  });
+  
+  // get the project types
+  const projectTypesArr = ["bimanueel", "VR"] // await dataSources().medAPI.getProjectTypes(); // still doesn't work, no errors but the function does not get deployed by firebase
+  let projectTypes = `enum ProjectType {`;
+  for(const projectType of projectTypesArr) {
+    projectTypes += projectType + `,`;
+  }
+  projectTypes += `}`; 
 
-// create the server with the schema, dataSources and the context
-const server = new ApolloServer({
-  schema,
-  context: async ({ req }) => {
-    
-    // get the token from the authorization header, or return an empty string
-    const token = req.headers.authorization || '';
-    // if no token throw authentication error
-    if(token == "") 
-      throw new AuthenticationError("No token provided");
-    // try to verify the token, and return a user object
-    try {
-      const userVerified = await getAuth().verifyIdToken(token);
+  // create schema, using type definitions in schema.ts, auth directive declaration from auth.ts, projectTypes enum and the resolvers
+  let schema = makeExecutableSchema({
+    typeDefs: [
+      authDirectiveTypeDefs,
+      queries,
+      mutations,
+      projectTypes
+    ],
+    resolvers,
+  });
+  // transform the schema so that the auth directive is implemented
+  schema = authDirectiveTransformer(schema);
+
+  // create the server with the schema, dataSources and the context
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
       
-      // create log if this is not an introspectionquery
-      if(req.body.operationName != Variables.INTRO_QUERY)
-        functions.logger.info("Access by " + userVerified.uid + ' (role: ' + userVerified.role +')', req.body);
-      return { 
-            user: {
-                uid: userVerified.uid,
-                role: userVerified.role
-            } 
-        };
-    } catch(error) {
-      throw new AuthenticationError("Invalid token");
-    }
-  },
-  // query for the schema? 
-  introspection: true, 
-  // apollo playground plugin
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(),
-  ],
-  dataSources, 
-});
+      // get the token from the authorization header, or return an empty string
+      const token = req.headers.authorization || '';
+      // if no token throw authentication error
+      if(token == "") 
+        throw new AuthenticationError("No token provided");
+      // try to verify the token, and return a user object
+      try {
+        const userVerified = await getAuth().verifyIdToken(token);
+        
+        // create log if this is not an introspectionquery
+        if(req.body.operationName != Variables.INTRO_QUERY)
+          functions.logger.info("Access by " + userVerified.uid + ' (role: ' + userVerified.role +')', req.body);
+        return { 
+              user: {
+                  uid: userVerified.uid,
+                  role: userVerified.role
+              } 
+          };
+      } catch(error) {
+        throw new AuthenticationError("Invalid token");
+      }
+    },
+    // query for the schema? 
+    introspection: true, 
+    // apollo playground plugin
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+    dataSources, 
+  });
 
-// creates the firebase function, runWith sets the secret from google secret manager to process.env.??. it contains the service account for the second database
-exports.v1 = functions.runWith({ secrets: [Variables.FIREBASE_SERVICE_ACCOUNT_GSM] }).region(Variables.REGION_GRAPHQL).https.onRequest(server.createHandler() as any);
+  // creates the firebase function, runWith sets the secret from google secret manager to process.env.??. it contains the service account for the second database
+  exports.v1 = functions.runWith({ secrets: [Variables.FIREBASE_SERVICE_ACCOUNT_GSM] }).region(Variables.REGION_GRAPHQL).https.onRequest(server.createHandler() as any);
+})()
 
 // On sign up.
 exports.processSignUp = functions.region(Variables.REGION_SIGNUP_PROCESS).auth.user().onCreate(async (user) => {
